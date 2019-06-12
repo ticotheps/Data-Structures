8f414eeaa523515b06dfbc29cd7b7abafcc1fcec
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-06-12 14:02:07 -0400

    Updates the notes3a.py file

--- heap/notes3a.py
+++ heap/notes3a.py
@@ -59,3 +59,41 @@
     #    => Remove the last element from the array, because we don't want multiple
     #       copies
     #    => Bubble up the element at index 0.
+    
+class Heap:
+    def __init__(self):
+        self.storage = []
+            
+    def insert(self, value):
+        pass
+    
+    def delete(self):
+        pass
+    
+    def get_max(self):
+        pass
+    
+    def get_size(self):
+        pass
+    
+    def _bubble_up(self, index):
+        #  Keep bubbling up until we've either reached the top of the heap
+        #  or we've reached a point where the parent is higher priority
+        while index > 0:
+            #  On a single bubble up iteration:
+            #  Get the parent index
+            parent = (index - 1) // 2 
+            #  Compare the child against the value of the parent
+            #  If the child's value is higher priority than it's parent value
+            if self.storage[index] > self.storage[parent]:
+                #  Swap them
+                self.storage[index], self.storage[parent] = self.storage[parent], self.storage[index]
+                #  Update the child's index to be the new index it is now at
+                index = parent
+            #  Otherwise, child is at a valid spot
+            else:
+            #  Stop bubbling up
+                break
+            
+    def _sift_down(self, index):
+        pass
